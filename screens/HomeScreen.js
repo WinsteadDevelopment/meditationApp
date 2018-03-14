272b8f06b44cfb2c8d9a241a4f550646a0c7f5ac
awinste <awinste@gmail.com>
2018-03-13 20:24:45 -0500

    display star based upon user completions

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,17 +13,37 @@ import { WebBrowser } from 'expo';
 import axios from 'axios';
 import { MonoText } from '../components/StyledText';
 
+const starImages = [
+  require('../assets/images/0star.png'),
+  require('../assets/images/1star.png'),
+  require('../assets/images/2star.png'),
+  require('../assets/images/3star.png'),
+  require('../assets/images/4star.png'),
+  require('../assets/images/5star.png'),
+  require('../assets/images/6star.png'),
+  require('../assets/images/7star.png'),
+  require('../assets/images/8star.png')
+]
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      completions: starImages[0],
+    };
+  }
   componentWillMount() {
     AsyncStorage.getItem('Token')
       .then(token => {
-        return axios.get('http://af4ec08e.ngrok.io/protected', { headers: { authorization: JSON.parse(token) } })
+        return axios.get(`${server}/userCompletions`, { headers: { authorization: JSON.parse(token) } })
       })
-      .then(res => {/*console.log(res)*/})
+      .then(res => {
+        console.log('response: ', res.data);
+        this.state.completions = starImages[JSON.parse(res.data)];
+      })
       .catch(err => console.error(err));
   }
 
@@ -33,7 +53,7 @@ export default class HomeScreen extends React.Component {
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
             <Image
-              source={require('../assets/images/8star.png')}
+              source={this.state.completions}
               style={styles.welcomeImage}
             />
           </View>
